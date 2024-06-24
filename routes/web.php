64b7967919ee5334c8b67e390bf8b7ef0538fde1
Dilah <115137729+Muhamad202110715128@users.noreply.github.com>
2024-06-24 12:24:59 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('Home');
+});
+Route::get('/transaksi', function () {
+    return view('Transaksi');
+});
+Route::get('/invite', function () {
+    return view('Invite');
+});
+Route::get('/set', function () {
+    return view('Set-profile');
+});
+Route::get('/prefence', function () {
+    return view('Set-prefence');
+});
+Route::get('/security', function () {
+    return view('Set-security');
+});
+Route::get('/about', function () {
+    return view('About');
+});
+Route::get('/add', function () {
+    return view('Add-ons');
+});
+Route::get('/invite-form', function () {
+    return view('Invite-form');
+});
+Route::get('/template', function () {
+    return view('Template');
+});
+Route::get('/paket', function () {
+    return view('Paket');
+});
+Route::get('/upload', function () {
+    return view('Upload-Foto');
+});
+Route::get('/mbanking', function () {
+    return view('Mbanking');
+});
+Route::get('/struck', function () {
+    return view('transfer');
+});
+Route::get('/type', function () {
+    return view('Invite-type');
+});
+Route::get('/data', function () {
+    return view('no-data');
+});
+Route::get('/proses', function () {
+    return view('proses');
+});
+
+// register
+Route::get('/register', function () {
+    return view('Register');
+});
+
+Route::get('/verifikasi', function () {
+    return view('Verifikasi');
+});
+
+Route::get('/packet', function () {
+    return view('Packet');
+});
+
+Route::get('/payment', function () {
+    return view('Payment');
+});
